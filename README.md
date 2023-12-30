1635b24ee6110d20bf9c77ddedbff3c062211388
pantpad <138708449+pantpad@users.noreply.github.com>
2023-12-30 15:47:10 +0100

    complete application

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-app-top
+
+## Set up a blank HTML document with the appropriate links to your JavaScript and CSS files.
+
+## Write the functions that hit the API.
+
+You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
---
+++ script.js
@@ -0,0 +1,39 @@
+const API_KEY = "a658fc69faf74c2c820205545232512";
+
+async function getData(QUERY) {
+  let url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${QUERY}&aqi=no`;
+  const response = await fetch(url, { mode: "cors" });
+  const data = await response.json();
+  if (!response.ok) {
+    //handleError
+    console.log(data.error.message);
+  }
+  return data;
+}
+
+const searchBar = document.getElementById("searchBar");
+const searchBtn = document.getElementById("searchBtn");
+const resultBoxContainer = document.getElementById("resultBoxContainer");
+
+async function getMeteo() {
+  let QUERY = searchBar.value;
+  if (QUERY == "") return;
+
+  createResultBox(await getData(QUERY));
+}
+
+function createResultBox(data) {
+  console.log(data);
+  let html = `<div class="resultBox">
+    <h2>${data.location.name}</h2>
+    <p>${data.current.temp_c}°C</p>
+    <p>Fells Like: ${data.current.feelslike_c} C</p>
+    <p>Humidity: ${data.current.humidity} %</p>
+    <p>Wind: ${data.current.wind_kph} kph</p>
+  </div>`;
+  console.log(html);
+  resultBoxContainer.innerHTML = "";
+  resultBoxContainer.innerHTML = html;
+}
+
+searchBtn.addEventListener("click", getMeteo);
